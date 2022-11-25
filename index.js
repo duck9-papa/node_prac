86e1ac6c948b594b8b25e41d64997342310bce3a
duck9-papa <107654769+duck9-papa@users.noreply.github.com>
2022-11-26 01:22:59 +0900

    fri

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('hello world')
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+  console.log(req)
+})
+
+app.get('/about', function (req, res) {
+  res.send('about')
+})
+
+app.listen(port,()=>{
+  console.log(`example port ${port}`)
+})
