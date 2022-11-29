91fd344dfb096942f381b81166b08134017a1eb6
duck9-papa <107654769+duck9-papa@users.noreply.github.com>
2022-11-30 06:26:33 +0900

    setting

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, set } from "firebase/database";
+
+const express = require('express');
+const app = express();
+var cors = require('cors');
+const port = 3000;
+
+
 
 var firebase = require('firebase');
 
@@ -27,5 +35,27 @@ const firebaseConfig = {
   messagingSenderId: MESSAGING_SENDER_ID,
   appId: APP_ID,
 };
+
+console.log(API_KEY)
+
+// 비울시 모든 요청에대해 허용
+app.use(cors());
+
+app.get('/', function (req, res) {
+  res.send('hello');
+});
+
+app.get('/sound/:name', function (req, res) {
+  const { name } = req.params;
+  const sound = { dog: '멍멍', cat: '야옹' };
+  name in sound
+    ? res.json({ sound: sound[name] })
+    : res.json({ sound: '알수없음' });
+});
+
+app.listen(port, () => {
+  console.log(`example port ${port}`);
+});
+
 
 console.log(API_KEY)
