5e0b9257d36ee32bd9285a9de342d223577d99b2
duck9-papa <107654769+duck9-papa@users.noreply.github.com>
2022-11-29 23:30:18 +0900

    add env

--- firebase.js
+++ firebase.js
@@ -1,13 +1,19 @@
-const firebase = require('firebase')
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
-  }
+// Your web app's Firebase configuration
+const API_KEY = process.env.API_KEY
+console.log(API_KEY)
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCXeYJ5M6IH71Xvwd3mjCtD1wZ6D7og2oE",
+//   authDomain: "nodeprac-5f8a4.firebaseapp.com",
+//   projectId: "nodeprac-5f8a4",
+//   storageBucket: "nodeprac-5f8a4.appspot.com",
+//   messagingSenderId: "173954019434",
+//   appId: "1:173954019434:web:f0d26571e0564fbb701a13"
+// };
 
-  let database = firebase.database()
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
